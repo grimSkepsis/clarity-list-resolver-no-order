774e8b63e98e290644f4b9bfc6cfbb76abe81cbd
grimSkepsis <developer.mikehofer@gmail.com>
2019-07-19 13:01:49 -0700

    3 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -12,30 +12,45 @@ import { Subject, Observable } from 'rxjs/Rx';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+    public modalOpen = true;
+    public highlightedItemId: number;
     public listItems: Array<DiffListItem> = [
-    new DiffListItem(0, "Age : Aggregate", "Age : Sensitive", SelectedDiffOption.NotSelected),
-    new DiffListItem(1, "Date of Birth : Aggregate", "Date of Birth : No Access", SelectedDiffOption.NotSelected),
-    new DiffListItem(2, "First Name  : Aggregate", "First Name : Sensitive", SelectedDiffOption.NotSelected),
-    new DiffListItem(3, "Eye Color : Aggregate", "Eye Color : Sensitive", SelectedDiffOption.NotSelected),
+    new DiffListItem(0, "Age : Aggregate", "Age : Sensitive", "Age", SelectedDiffOption.NotSelected),
+    new DiffListItem(1, "Date of Birth : Aggregate", "Date of Birth : No Access", "Date of Birth", SelectedDiffOption.NotSelected),
+    new DiffListItem(2, "First Name  : Aggregate", "First Name : Sensitive", "First Name", SelectedDiffOption.NotSelected),
+    new DiffListItem(3, "Eye Color : Aggregate", "Eye Color : Sensitive", "Eye Color", SelectedDiffOption.NotSelected),
 
-    new DiffListItem(10, "Vacation Days : Aggregate", "Vacation Days : No Access", SelectedDiffOption.NotSelected),
-    new DiffListItem(4, "Hair Color : Aggregate", "Hair Color : No Access", SelectedDiffOption.NotSelected),
-    new DiffListItem(5, "Last Name  : Aggregate", "Last Name : Sensitive", SelectedDiffOption.NotSelected),
-    new DiffListItem(6, "Height : Aggregate", "Height : Sensitive", SelectedDiffOption.NotSelected),
-    new DiffListItem(7, "Salary : Aggregate", "Salary : No Access", SelectedDiffOption.NotSelected),
-    new DiffListItem(8, "Employee ID  : Aggregate", "Employee ID : Sensitive", SelectedDiffOption.NotSelected),
-    new DiffListItem(9, "Gender: Aggregate", "Gender : Sensitive", SelectedDiffOption.NotSelected),
-    new DiffListItem(11, "Marital Status  : Aggregate", "Marital Status : Sensitive", SelectedDiffOption.NotSelected),
+    new DiffListItem(10, "Vacation Days : Aggregate", "Vacation Days : No Access", "Vacation Days", SelectedDiffOption.NotSelected),
+    new DiffListItem(4, "Hair Color : Aggregate", "Hair Color : No Access", "Hair Color", SelectedDiffOption.NotSelected),
+    new DiffListItem(5, "Last Name  : Aggregate", "Last Name : Sensitive", "Last Name", SelectedDiffOption.NotSelected),
+    new DiffListItem(6, "Height : Aggregate", "Height : Sensitive",  "Height", SelectedDiffOption.NotSelected),
+    new DiffListItem(7, "Salary : Aggregate", "Salary : No Access", "Salary", SelectedDiffOption.NotSelected),
+    new DiffListItem(8, "Employee ID  : Aggregate", "Employee ID : Sensitive", "Employee ID", SelectedDiffOption.NotSelected),
+    new DiffListItem(9, "Gender: Aggregate", "Gender : Sensitive", "Gender", SelectedDiffOption.NotSelected),
+    new DiffListItem(11, "Marital Status  : Aggregate", "Marital Status : Sensitive", "Marital Status", SelectedDiffOption.NotSelected),
   ];
 
     public resultsList: Array<DiffListItem> = this.listItems;
     public confirmationList: Array<DiffListItem> = [];
     public isClearSelectionVisible: boolean = false;
-  modalOpen = true;
-  public highlightedItemId: number;
+    public showingMoreInfo: boolean = false;
+    public moreInfoTitle: string;
+
+    public onShowMoreInfo(event: Event, id: number): void {
+      this.showingMoreInfo = true;
+      event.stopPropagation();
+      this.moreInfoTitle = this._findItemById(id).title;
+    } 
+
+    public onCloseMoreInfo(): void {
+      this.showingMoreInfo = false;
+    }
+
+private _findItemById(id: number): DiffListItem {
+ return this.resultsList.filter(item => item.id === id)[0];
+}
   public removeSelection(id: number): void {
-        let selectedItem: DiffListItem = this.resultsList.filter(item => item.id === id)[0];
-        selectedItem.selectedOption = SelectedDiffOption.NotSelected;
+        this._findItemById(id).selectedOption = SelectedDiffOption.NotSelected;
         this._updateConfirmationList();
         this._updateClearSelectionVisibility();
   }
@@ -92,7 +107,9 @@ export class DiffListItem {
   constructor(public readonly id: number,
     public readonly leftOption: string,
     public readonly rightOption: string,
-    public selectedOption: SelectedDiffOption) { }
+    public readonly title: string,
+    public selectedOption: SelectedDiffOption,
+) { }
 
     public get rightSideSelected(): boolean {
       return this.selectedOption === SelectedDiffOption.RightOption;
